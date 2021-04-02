41a651f3c54ff466c9599fc4c5c304ac90e16586
cyleung428 <msf96a85@gmail.com>
2021-04-03 00:03:20 +0800

    UI: display storyList on buttons

--- scrum-poker/src/App.tsx
+++ scrum-poker/src/App.tsx
@@ -1,4 +1,4 @@
-import { ITextFieldStyles, mergeStyles, PrimaryButton, Stack, TextField } from '@fluentui/react';
+import { CompoundButton, ITextFieldStyles, mergeStyles, PrimaryButton, Stack, TextField } from '@fluentui/react';
 import { CompatClient, Message, Stomp } from '@stomp/stompjs';
 import React, { useState } from 'react';
 import SockJS from 'sockjs-client';
@@ -14,9 +14,22 @@ const appStyles = mergeStyles({
 })
 const stackTokens = { childrenGap: 15 };
 const textFieldStyles: Partial<ITextFieldStyles> = {};
+interface IMessage {
+    content: any,
+    type: String,
+    sender: String
+}
+interface IStory {
+    id: Number,
+    storyName: String,
+    active: Boolean
+}
+
 function App() {
     const [connect, setConnect] = useState(false);
     const [email, setEmail] = useState("");
+    const [storyList, setStoryList] = useState([] as IStory[]);
+    const [activeID, setActiveID] = useState(-1);
     const [stompClient, setStompClient] = useState<CompatClient>();
 
     const onChangeEmail = React.useCallback(
@@ -29,11 +42,18 @@ function App() {
     const connectToServer = () => {
         const socket = new SockJS(`${baseUrl}/chat`);
         const stompClient = Stomp.over(socket);
-        const onMessageReceived = (payload:Message) => {
-            const message = JSON.parse(payload.body);
+        const onMessageReceived = (payload: Message) => {
+            const message: IMessage = JSON.parse(payload.body);
             console.log(message);
+            const content = message.content;
+            switch (message.type) {
+                case "GETSTORY":
+                    setActiveID(content.activeID);
+                    setStoryList(content.storyList);
+                    break;
+            }
         }
-    
+
         const onConnected = () => {
             stompClient.subscribe('/topic/public', onMessageReceived)
             stompClient.send("/app/chat.newUser",
@@ -41,33 +61,59 @@ function App() {
                 JSON.stringify({ sender: email, type: 'CONNECT' })
             )
             setConnect(true);
+            setStompClient(stompClient);
         }
-    
+
         const onError = () => {
-    
+
         }
         stompClient.connect({}, onConnected, onError)
+    }
+
+    const onSelectStory = (id: Number) => {
+        console.log(id);
     }
     return (
         <div className={appStyles}>
             <Stack tokens={stackTokens}>
-            {
-                connect ?
-                    <>
-                    <span>Email: {email}</span>
-                    </>
-                    :
-                    <>
-                        <TextField
-                            label="Type your Email"
-                            value={email}
-                            onChange={onChangeEmail}
-                            styles={textFieldStyles}
-                        />
-                        <PrimaryButton text="Connect" onClick={connectToServer} />
-                    </>
+                {
+                    connect ?
+                        <>
+                            <span>Email: {email}</span>
+                            {
+                                activeID === -1 ?
+                                    <>
+                                        {
+                                            storyList.map(story => (
+                                                <CompoundButton
+                                                    primary
+                                                    key={`${story.id}`}
+                                                    secondaryText={`ID: ${story.id}`}
+                                                    onClick={() => { onSelectStory(story.id) }}
+                                                >
+                                                    {story.storyName}
+                                                </CompoundButton>
+                                            ))
+                                        }
+                                    </>
+                                    :
+                                    <>
 
-            }
+                                    </>
+                            }
+                        </>
+                        :
+                        <>
+                            <TextField
+                                label="Type your Email"
+                                value={email}
+                                onChange={onChangeEmail}
+                                styles={textFieldStyles}
+                            />
+                            <PrimaryButton text="Connect" onClick={connectToServer} />
+                        </>
+
+                }
             </Stack>
         </div>
     );
